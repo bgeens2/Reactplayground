8d8dfa72d32aa0313f8cb2ef08a577267819fd37
bgeens2 <bg@ipng.be>
2019-08-07 18:13:28 +0200

    Dynamic mdx component
    Mdx

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Grommet, Box } from "grommet";
 
 import "./styles.css";
 
+import Mdxtest from "./mdxtest";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Grommet>
+      <div className="App">
+        <h1>Hello CodeSandbox...</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <Mdxtest />
+      </div>
+    </Grommet>
   );
 }
 
---
+++ src/mdxtest.js
@@ -0,0 +1,29 @@
+import MDX from "@mdx-js/runtime";
+import React from "react";
+
+// Provide custom components for markdown elements
+const components = {
+  h1: props => <h1 style={{ color: "tomato" }} {...props} />,
+  Demo: props => <h1>This... is a demo component</h1>
+};
+
+// Provide variables that might be referenced by JSX
+const scope = {
+  some: "value"
+};
+
+const mdx = `
+# Hello, world!
+
+- go
+- test 
+- mdx
+
+<Demo />
+`;
+
+export default () => (
+  <MDX components={components} scope={scope}>
+    {mdx}
+  </MDX>
+);
